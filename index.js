3be6f9bdb1c4fb2af5a6358fa0bfde5241bcd436
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-25 09:36:42 -0300

    01:06 -  Como excluir propriedades e métodos

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
 const carro = {
-    marca: "Toyota",
-    modelo: "Corola",
+    marca: 'Toyota',
     ano: 2023,
     proprietario: {
         nome: 'Ariel',
-        editarNome() {
-            // o this representa o objeto pai, no caso o proprietario
-
-
-            // carro.marca = 'Fiat';
-            // carro.proprietario.nome = 'Carlos';
-            // this.nome = 'Pedro';
-        }
+        idade: 25,
     },
-    "chassi": '123456',
-    "@": "@@",
     ligar() {
         console.log('O carro ligou');
-    },
-    desligar() {
-        console.log('O carro desligou');
     }
 }
-carro.ano = 2024;
-carro.ligar = () => {
-    console.log('Novo método: O carro ligou');
-}
 
-carro['proprietario'] = 'Pedro';
-carro['chassi'] = '456';
+delete carro['proprietario']['nome'];
+delete carro.proprietario;
+delete carro.marca;
+delete carro['ligar'];
 
-carro['@'] = 'novo @';
 
 console.log(carro);
-
-

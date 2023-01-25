d8f5ce3681f92f3526b5e0b594b26b7ec110affa
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-01-25 09:34:04 -0300

    01:02 - Como criar Objeto

---
+++ index.js
@@ -0,0 +1,11 @@
+// const novoObjeto = undefined;
+
+// Objeto JavaScript
+// {chave: valor}
+// Criando pelo construtor
+const forma1 = new Object();
+// Criando pela notação literal
+const forma2 = {};
+
+console.log(typeof forma1);
+console.log(typeof forma2);
